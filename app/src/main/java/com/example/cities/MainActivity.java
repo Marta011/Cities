22e4258da8bbc7b53509b2aa412661638df8b814
Marta - nowe <martaowczarczuk@gmail.com>
2020-08-16 17:43:23 +0200

    Formatting code in MainActivity.java

--- app/src/main/java/com/example/cities/MainActivity.java
+++ app/src/main/java/com/example/cities/MainActivity.java
@@ -1,51 +1,38 @@
 package com.example.cities;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
 import android.view.Menu;
+import android.view.MenuItem;
 
-import com.example.cities.helpers.DbOperations;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
+import com.example.cities.helpers.DbOperations;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private NavController navController;
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.inflateMenu(R.menu.menu);
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_city)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-
+        attributeWidgets();
+        setUpToolbar();
+        setUpAppBarConfiguration();
+        setUpNavigationUI();
         setUpMenuOptionActions();
     }
-
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -65,9 +52,9 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                if(item.getItemId()==R.id.menu_populateDb_Location)
+                if (item.getItemId() == R.id.menu_populateDb_Location)
                     DbOperations.populateDb_locationsTable(getApplicationContext());
-                else if (item.getItemId()==R.id.menu_populateDb_Population)
+                else if (item.getItemId() == R.id.menu_populateDb_Population)
                     DbOperations.populateDb_populationsTable(getApplicationContext());
                 else
                     DbOperations.populateDb_citiesTable(getApplicationContext());
@@ -76,4 +63,29 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    void setUpAppBarConfiguration() {
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_city)
+                .setDrawerLayout(drawer)
+                .build();
+    }
+
+    void setUpNavigationUI() {
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    void setUpToolbar() {
+        setSupportActionBar(toolbar);
+        toolbar.inflateMenu(R.menu.menu);
+    }
+
+    void attributeWidgets() {
+        toolbar = findViewById(R.id.toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+    }
 }
